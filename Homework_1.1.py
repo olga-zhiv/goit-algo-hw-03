4093adcab85758fa13c0e35cc6cf3ee3559b713e
olga-zhiv <olga-zhiv@ukr.net>
2025-03-28 21:32:35 +0200

    Update Homework_1.1.py

--- Homework_1.1.py
+++ Homework_1.1.py
@@ -14,7 +14,7 @@ def get_days_from_today(date):  # Оголошуємо функцію, яка з
 
     try:   # Обробка можливих помилок вводу дати
 
-        datetime_object = datetime.strptime(date_string, "%Y.%m.%d").date()  # Форматування дати із строки в об'єкт
+        datetime_object = datetime.strptime(date_string, "%Y-%m-%d").date()  # Форматування дати із строки в об'єкт
 
         new_date = (datetime_object - current_datetime).days    # Обчислення різниці дат
 
@@ -22,10 +22,10 @@ def get_days_from_today(date):  # Оголошуємо функцію, яка з
 
 
     except ValueError: 
-        print("Введіть дату y вірному форматі: %Y.%m.%d")    # Якщо формат введення дати невірний, то на екрані з'явиться відповідний запис
+        print("Введіть дату y вірному форматі: %Y-%m-%d")    # Якщо формат введення дати невірний, то на екрані з'явиться відповідний запис
 
 
 
 
-days = get_days_from_today("2025.03.29")  # Виклик функції
+days = get_days_from_today("2025-03-29")  # Виклик функції
 print(days)
